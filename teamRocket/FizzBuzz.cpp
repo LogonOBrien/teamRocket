1aa02f52d398707e7994fd57ff509f85467a9023
Kien Pham <KTPham@student.fullsail.edu>
2026-08-12 15:17:26 -0400

    Add prime number feature and adjust FizzBuzz output flow

--- teamRocket/FizzBuzz.cpp
+++ teamRocket/FizzBuzz.cpp
@@ -1,19 +1,62 @@
 #include <iostream>
 
 int main() {
-    for (int i = 1; i <= 100; ++i) {
-        if (i % 3 == 0 && i % 5 == 0) {
+    for (int i = 1; i <= 100; ++i) 
+    {
+        
+        // check whether the current number is prime
+        bool isPrime = true;
+
+        if (i < 2) 
+        {
+            isPrime = false;
+        }
+        else 
+        {
+            for (int j = 2; j < i; ++j) 
+            {
+                if (i % j == 0) 
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+
+        // Kien's feature:
+        // Prime numbers output one character instead of the normal FizzBuzz result
+        if (isPrime) 
+        {
+            std::cout << "P\n";
+        }
+        else if (i % 3 == 0 && i % 5 == 0) // divisible by both -> FizzBuzz
+        {
             std::cout << "FizzBuzz\n";
         }
-        else if (i % 3 == 0) {
+        else if (i % 3 == 0) // divisible by 3 -> Fizz
+        {
             std::cout << "Fizz\n";
         }
-        else if (i % 5 == 0) {
+        else if (i % 5 == 0) // divisible by 5 -> Buzz
+        {
             std::cout << "Buzz\n";
         }
-        else {
+        else 
+        {
             std::cout << i << "\n";
         }
     }
+
     return 0;
 }
+
+
+
+
+
+// divisible by 3 → Fizz, 
+//           by 5 → Buzz, 
+//           by both → FizzBuzz.
+// ## Task:
+// If any loop's iteration is a prime number only output 1 character instead of what ever was going to be output that loop.
