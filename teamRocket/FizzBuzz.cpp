2b6c8579c59209f3c015f16135e2301d19bfe7d6
ndhardy395 <notredamehardy@gmail.com>
2026-08-12 15:26:59 -0400

    More confident with this implementation, added switch statement, but it's still buggy

--- teamRocket/FizzBuzz.cpp
+++ teamRocket/FizzBuzz.cpp
@@ -1,33 +1,58 @@
 #include <iostream>
 
 int main() {
-    char uppercase = 'A';
-    char lowercase = 'a';
-    std::cout << static_cast<char>(uppercase + 1) << std::endl;
-    std::cout << static_cast<char>(lowercase + 1) << std::endl;
+	int FCount = 0;
+	int ICount = 0;
+	int BCount = 0;
+	int UCount = 0;
+	int ZCount = 0;
+	char lowercase = 'a';
 
-    for (int i = 1; i <= 26; i++)//26 -> number of letter in the alphabet
-    {
-        char outerLetter = lowercase + i;
-        for (int i = 1; i <= 26; i++)
-        {
-            char innerLetter = uppercase + i;
-        }
-    }
 
-    for (int i = 1; i <= 100; ++i) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            std::cout << "FizzBuzz\n";
-        }
-        else if (i % 3 == 0) {
-            std::cout << "Fizz\n";
-        }
-        else if (i % 5 == 0) {
-            std::cout << "Buzz\n";
-        }
-        else {
-            std::cout << i << "\n";
-        }
-    }
-    return 0;
+	for (int i = 1; i <= 100; ++i) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			std::cout << "FizzBuzz\n";
+			FCount += 1;
+			ICount += 1;
+			BCount += 1;
+			UCount += 1;
+			ZCount += 4;
+		}
+		else if (i % 3 == 0) {
+			std::cout << "Fizz\n";
+			FCount += 1;
+			ICount += 1;
+			ZCount += 2;
+		}
+		else if (i % 5 == 0) {
+			std::cout << "Buzz\n";
+			BCount += 1;
+			UCount += 1;
+			ZCount += 2;
+		}
+		else {
+			std::cout << i << "\n";
+		}
+		for (int i = 0; i < 26; i++)//26 -> number of letter in the alphabet
+		{
+			int value = 0;
+			switch (lowercase + i)
+			{
+			case 'f': value = FCount;
+				break;
+			case 'i': value = ICount;
+				break;
+			case 'b': value = BCount;
+				break;
+			case 'u': value = UCount;
+				break;
+			case 'z': value = ZCount;
+				break;
+			default: break;
+			}
+				std::cout << static_cast<char>(lowercase + i) << ' -> ' << value << std::endl;
+				value = 0;
+		}
+	}
+	return 0;
 }
