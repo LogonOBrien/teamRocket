ea344f4c75f1fd0e46c9e0c25e0c57ba2436d016
ndhardy395 <notredamehardy@gmail.com>
2026-08-12 14:53:42 -0400

    Added the basic elements of my challenge. A nester for loop, and some declerations

--- teamRocket/FizzBuzz.cpp
+++ teamRocket/FizzBuzz.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
 int main() {
+    char uppercase = 'A';
+    char lowercase = 'a';
+    std::cout << static_cast<char>(uppercase + 1) << std::endl;
+    std::cout << static_cast<char>(lowercase + 1) << std::endl;
+
+    for (int i = 1; i <= 26; i++)//26 -> number of letter in the alphabet
+    {
+        char outerLetter = lowercase + i;
+        for (int i = 1; i <= 26; i++)
+        {
+            char innerLetter = uppercase + i;
+        }
+    }
+
     for (int i = 1; i <= 100; ++i) {
         if (i % 3 == 0 && i % 5 == 0) {
             std::cout << "FizzBuzz\n";
