eb29b295b8505594be5b3e69659348a23236dbfd
Logon OBrien <ljobrien@student.fullsail.edu>
2026-08-12 15:07:48 -0400

    make file clean and moved so code to make program work

--- teamRocket/10thloop.cpp
+++ teamRocket/10thloop.cpp
@@ -1,6 +1,2 @@
-for (int i = 1; i <= 100; ++i) {
-	if (i % 10 == 0) {
-		std::cout << i << "10x" << std::endl;
-	}
-}
+
 
--- teamRocket/FizzBuzz.cpp
+++ teamRocket/FizzBuzz.cpp
@@ -24,6 +24,10 @@ int main() {
 		}
 
 	}
-	
+	for (int i = 1; i <= 100; ++i) {
+	if (i % 10 == 0) {
+		std::cout << i << "10x" << std::endl;
+	}
+}
 	return 0;
 }
