e40fd476be4d8e135d191feaff3437e558cbab06
Huy Pham <HDPham1@student.fullsail.edu>
2026-08-12 15:08:03 -0400

    Add random iteration skip

--- teamRocket/FizzBuzz.cpp
+++ teamRocket/FizzBuzz.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
+
+    srand(time(0));
+    int skipNumber = rand() % 100 + 1;
+
     for (int i = 1; i <= 100; ++i) {
+
+        if (i == skipNumber) {
+            continue;
+        }
+
         if (i % 3 == 0 && i % 5 == 0) {
             std::cout << "FizzBuzz\n";
         }
@@ -15,5 +26,6 @@ int main() {
             std::cout << i << "\n";
         }
     }
+
     return 0;
 }
