5f6cb15e8dd3b85431b7a879d092f6e1cac23e6e
drauka2 <drauka2@gmail.com>
2026-08-12 14:29:33 -0400

    Garret todo finished

--- teamRocket/FizzBuzz.cpp
+++ teamRocket/FizzBuzz.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    for (int call = i; call <= 100; call++){
+    for (int call = 1; call <= 100; call++){
         std::string output = "";
-        if (i % 3 == 0) output += "Fizz";
-        if (i % 5 == 0) output += "Buzz";
+        if (call % 3 == 0) output += "Fizz";
+        if (call % 5 == 0) output += "Buzz";
 
         std::cout << (output.empty() ? std::to_string(call) : output); // output call 1
         std::cout << "\n"; // output call 2
